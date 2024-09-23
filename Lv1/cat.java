bf95286e38f36a6d93b834c8a8a355b22ebf14f2
tjfgk97 <tjfgk97@gmail.com>
2024-09-24 04:01:40 +0900

    10171번

---
+++ Lv1/cat.java
@@ -0,0 +1,11 @@
+package Lv1;
+
+public class cat {
+    public static void main(String[] args) {
+
+       System.out.println("\\    /\\");
+       System.out.println(" )  ( ')");
+       System.out.println("(  /  ) ");
+       System.out.println(" \\(__)| ");
+    }
+}
